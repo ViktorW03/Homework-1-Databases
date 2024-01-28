55fe177453b00c4dcf694af213dc4f3a090a2a3f
Viktor Wardum <viktorwardum@gmail.com>
2024-01-28 13:17:04 +0000

    Adding the assignment

---
+++ HW1-TEMPLATE.sql
@@ -0,0 +1,57 @@
+-- HW1
+-- Student names:
+
+-- A. How much does the most expensive equipment cost? Return only the price.
+-- Explanation: 
+
+
+
+-- B. 792 members have started the gym in April (of any year). How many members have started the gym in January (of any year)?
+-- Explanation: 
+
+
+
+-- C. 154 classes were held with ‘burn’ somewhere in their type name. How many classes were held that 
+-- have ‘fit’ somewhere in their type name? (Note that your query should be case-insensitive, 
+-- i.e. classes with ‘fiT’ and ‘Fit’ in their type name should also be counted).
+-- Explanation: 
+
+
+
+-- D. How many different instructors have led at least one class in which a member that they have in personal training attended?
+-- Explanation: 
+
+
+
+-- E. Return the name of every class type along with the average rating that all classes of the type have received. 
+-- The result should be rounded to the nearest integer and ordered from highest to lowest. 
+-- Name the column with the average rating “Average Rating”.
+-- Explanation: 
+
+
+
+-- F. How many members have not attended any classes and do not have a personal instructor?
+-- Explanation: 
+
+
+
+-- G. 43 instructors have led 15 or more classes. How many instructors have led 10 or more classes?
+-- Explanation: 
+
+
+
+-- H. For how many members is it true that there exists at least one other member with the same start date and quit date as them? 
+-- (Note that if that is true for John and Mary, they should be counted as two results.
+-- Note also that two people that have not quit cannot be considered as having the same quit date.).
+-- Explanation: 
+
+
+
+--I. How many classes were held in gyms in Reykjavik and have a capacity of either 30 or 40 people, but the capacity was not used fully?
+-- Explanation: 
+
+
+
+-- J. Return the ID and name of the member(s) that attended classes for the longest total time (in minutes) of all members?
+-- Explanation: 
+
