b3baf737d9f64da6354f37b6a1258310930eb485
totsss <axetra@hotmail.com>
2024-01-31 14:42:45 +0000

    Update HW1-Queries.sql ---C

--- HW1-Queries.sql
+++ HW1-Queries.sql
@@ -6,7 +6,8 @@ SELECT start_date
 FROM Member
 WHERE EXTRACT(MONTH FROM start_date) = 1;
 -- C
-
+SELECT * FROM type WHERE name like '%fit%' OR name like 'fit%' OR name like '%Fit' or name like '%fiT%';
+select * from class where tid='8' or tid = '14' or tid = '15';
 -- D
 -- E
 -- F
