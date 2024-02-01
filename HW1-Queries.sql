215762a5b19e5d71ce714465c22d24aa491c7727
IsakT22 <142887350+IsakT22@users.noreply.github.com>
2024-02-01 10:16:50 +0000

    Spurning E

--- HW1-Queries.sql
+++ HW1-Queries.sql
@@ -1,15 +1,21 @@
--- A
+-- A --
 SELECT MAX(price) 
 FROM Equipment;
--- B
+-- B --
 SELECT start_date
 FROM Member
 WHERE EXTRACT(MONTH FROM start_date) = 1;
--- C
+-- C --
 SELECT * FROM type WHERE name like '%fit%' OR name like 'fit%' OR name like '%Fit' or name like '%fiT%';
 select * from class where tid='8' or tid = '14' or tid = '15';
 -- D
--- E
+-- E --
+SELECT Type.name, ROUND(AVG(Attends.rating)) AS "Average Rating"
+FROM Type
+JOIN Class ON Type.ID = Class.TID
+LEFT JOIN Attends ON Class.ID = Attends.CID
+GROUP BY Type.name
+ORDER BY "Average Rating" DESC;
 -- F
 -- G
 -- H
@@ -20,7 +26,7 @@ SELECT Member.ID, Member.name, MAX(Class.minutes) AS total_time
 FROM Class
 INNER JOIN Member ON Class.ID = Member.ID
 GROUP BY Member.ID, Member.name
-ORDER BY total_time DESC
+ORDER BY total_time DESC;
 -- All 90 min members --
 SELECT Member.ID, Member.name, MAX(Class.minutes) AS total_time
 FROM Class
