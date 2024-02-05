ea00dfe64af4b062bf70172e5c5b743f186014a9
IsakT22 <142887350+IsakT22@users.noreply.github.com>
2024-02-05 14:20:49 +0000

    Update HW1-Queries.sql

--- HW1-Queries.sql
+++ HW1-Queries.sql
@@ -1,20 +1,24 @@
 -- A --
 SELECT MAX(price) 
 FROM Equipment;
+
 -- B --
 SELECT start_date
 FROM Member
 WHERE EXTRACT(MONTH FROM start_date) = 1;
+
 -- C --
 SELECT * FROM type WHERE LOWER(name) like '%fit%';
 
 select * from class where tid='8' or tid = '14' or tid = '15' OR tid = '12';
--- D
+
+-- D --
 SELECT COUNT(DISTINCT Class.IID)
 FROM Class
 JOIN Attends ON Class.ID = Attends.CID
 JOIN Member ON Attends.MID = Member.ID
 WHERE Class.IID = Member.IID;
+
 -- E --
 SELECT Type.name, ROUND(AVG(Attends.rating)) AS "Average Rating"
 FROM Type
@@ -22,21 +26,22 @@ JOIN Class ON Type.ID = Class.TID
 LEFT JOIN Attends ON Class.ID = Attends.CID
 GROUP BY Type.name
 ORDER BY "Average Rating" DESC;
--- F
+
+-- F --
 SELECT COUNT(DISTINCT Member.ID)
 FROM Member
 LEFT JOIN Attends ON Member.ID = Attends.MID
 WHERE Attends.MID IS NULL
 AND IID IS NULL;
--- G
+
+-- G --
 SELECT instructor.id, COUNT(*) as id_count
 FROM instructor
 JOIN class ON class.iid = instructor.id
 GROUP BY instructor.id
 HAVING COUNT(*) > 14;
 
-
--- H
+-- H --
 select sum(count)
 from (
 
@@ -47,7 +52,7 @@ having count(*)>1
 
 );
  
--- I
+-- I -- 
 SELECT COUNT(DISTINCT Class.ID)
 FROM Class
 JOIN Gym ON Class.GID = Gym.ID
@@ -57,7 +62,7 @@ WHERE Gym.address Like '%Reykjavik%'
 AND Type.capacity in (30, 40)
 AND (SELECT COUNT(*) FROM Attends WHERE Attends.CID = Class.ID) < Type.capacity;
 
--- J
+-- J --
 -- All members --
 SELECT Member.ID, Member.name, MAX(Class.minutes) AS total_time
 FROM Class
